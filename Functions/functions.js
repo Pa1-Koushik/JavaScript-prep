879cfded4d4fcaa475d8776f8ee5d9b3a4ca1933
Pavan <koushikpavan99@gmail.com>
2025-11-23 18:25:03 +0530

    functions and arrow functions practice; create README file

---
+++ Functions/functions.js
@@ -0,0 +1,45 @@
+
+// function square (a){
+//     sq=a*a
+// }
+// square(5)
+// console.log(sq)
+
+// function multiply (a,b){
+//     // result = a*b
+//     // console.log(result)
+//     return a*b
+// }
+// let ans = multiply(5,4)
+// console.log(ans)
+
+// Another way of declaring a function
+
+// let addition = function (a,b){
+//     return a+b
+// }
+// ans =addition(6,6)
+// console.log(ans)
+
+//Arrow functions 
+
+// let exp = (a,b)=>{
+//     return a**b
+// }
+// console.log(exp(2,10))
+
+// const multiply = (a,b) =>a * b;
+// console.log(multiply(5,5))
+
+
+
+// let concat =(name1,name2)=>{
+//     return name1+name2
+// }
+// console.log(concat("pavan"," koushik"))
+
+// let add = function (a,b){
+//     sum=a+b
+//     console.log(sum)
+// }
+// add(5,5)
