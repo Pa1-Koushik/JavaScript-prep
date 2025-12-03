f4db055015f683b498824d1d8dc622f750413278
Pavan <koushikpavan99@gmail.com>
2025-12-03 22:52:40 +0530

    practiced destructuring (arrays & objects)

---
+++ ES6_Concepts/ES.js
@@ -0,0 +1,57 @@
+//object destructuring
+// const employee = {
+//     id:1,
+//     name:"Koushik",
+//     age:25,
+//     department:"development",
+//     address:{
+//         city:"vizag",
+//         country:"India"
+//     }
+
+// }
+
+// const name = employee.name
+// const age = employee.age
+
+// const {name:fullname = "pannu",age,id} = employee
+
+// const {address:{country}} = employee
+
+// console.log(country)
+// console.log(fullname,id)
+// console.log(age)
+
+// arrays 
+
+// const fruits = ["apple","banana","mango","kiwi"]
+
+// const f1=fruits[0]
+// console.log(f1)
+
+// const [item1,,item3] = fruits
+// console.log(item1,item3)
+
+// const [i1,...remaining] = fruits
+// console.log(remaining)
+
+//array and object destructuring
+
+// const users = [
+//     { name: "John", age: 25 },
+//     { name: "Kumar", age: 30 }
+// ];
+
+// const [,{name:fullname}] = users;
+// console.log(fullname)
+
+
+// const company = {
+//     cname:'mg',
+//     locations:["india","usa","australia"]
+// }
+
+// const {locations:[,loc]} = company
+// console.log(loc)
+
+
